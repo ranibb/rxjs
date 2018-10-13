ebff14c251921342989999f1be1966b4586a0d9c
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-13 22:49:53 +0300

    Building Components with RxJs - Imperative Design

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -15,24 +15,6 @@ export class AboutComponent implements OnInit {
 
     ngOnInit() {
 
-        const http$ = createHttpObservable('api/courses');
-
-        const courses$ = http$.pipe(
-            // Obtain the value of payload can be done in 2 ways:
-
-            // 1 some way
-            // map((res:any) => res.payload)
-
-            // 2 standard JavaScript 
-            map(res => Object.values(res['payload']))
-        );
-
-        courses$.subscribe(
-            courses => console.log(courses),
-            noop,
-            () => console.log('completed')
-        );
-
     }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {interval, Observable, of, timer} from 'rxjs';
-import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { noop } from 'rxjs';
+import { map } from 'rxjs/operators';
 
+import { Course } from "../model/course";
+import { createHttpObservable } from '../common/util';
 
 @Component({
     selector: 'home',
@@ -11,6 +12,8 @@ import {catchError, delayWhen, map, retryWhen, shareReplay, tap} from 'rxjs/oper
 })
 export class HomeComponent implements OnInit {
 
+    beginnersCourses: Course[];
+    advancedCourses: Course[];
 
     constructor() {
 
@@ -18,7 +21,29 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
 
+        const http$ = createHttpObservable('api/courses');
 
+        const courses$ = http$.pipe(
+            // Obtain the value of payload can be done in 2 ways:
+
+            // 1 some way
+            // map((res:any) => res.payload)
+
+            // 2 standard JavaScript 
+            map(res => Object.values(res['payload']))
+        );
+
+        courses$.subscribe(
+            courses => {
+
+                this.beginnersCourses = courses.filter(course => course.category == 'BEGINNER');
+
+                this.advancedCourses = courses.filter(course => course.category == 'ADVANCED');
+
+            },
+            noop,
+            () => console.log('completed')
+        );
 
     }
 
