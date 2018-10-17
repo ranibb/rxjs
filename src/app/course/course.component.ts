1f7f4fc39f836cb147ab91b78f8242ab15a7c4ac
Rani Abu Sultan <raniabusultan@gmail.com>
2018-10-17 12:06:32 +0300

    forking two streams and then we are joining the results of the streams together when both requests are completed.

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -13,7 +13,7 @@ import {
     withLatestFrom,
     concatAll, shareReplay
 } from 'rxjs/operators';
-import { merge, fromEvent, Observable, concat } from 'rxjs';
+import { merge, fromEvent, Observable, concat, forkJoin } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { createHttpObservable } from '../common/util';
 import { debug, RxJsLoggingLevel, SetRxJsLoggingLevel } from '../common/debug';
@@ -42,13 +42,18 @@ export class CourseComponent implements OnInit, AfterViewInit {
 
         this.courseId = this.route.snapshot.params['id'];
 
-        this.course$ = createHttpObservable(`/api/courses/${this.courseId}`)
-            .pipe(
-                debug(RxJsLoggingLevel.INFO, "course value")
-            )
+        const course$ = createHttpObservable(`/api/courses/${this.courseId}`);
 
-        SetRxJsLoggingLevel(RxJsLoggingLevel.TRACE)
-        // SetRxJsLoggingLevel(RxJsLoggingLevel.DEBUG)
+        const lessons$ = this.loadLessons();
+
+        forkJoin(course$, lessons$)
+        .pipe(
+            tap(([course, lessons]) => {
+                console.log('course', course);
+                console.log('lessons', lessons);
+            })
+        )
+        .subscribe()
 
     }
 
